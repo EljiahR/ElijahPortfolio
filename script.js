b8192f8982b1612e75dc49de5f00b377d7e8ea21
EljiahR <142916290+EljiahR@users.noreply.github.com>
2024-07-06 17:53:40 +0000

    Made cabinet look a little more like a cabinet, used js for some click functionality

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 const r = document.querySelector(":root");
-const cabinetClosedPosition = "0px";
+const cabinetClosedPosition = "-430px";
 const cabinetOpenPosition = "100px";
-let cabinetIsOpen = true;
-const cabinetFront = document.getElementById("cabinet-front");
-cabinetFront.addEventListener("click", () => {
-  r.style.setProperty("--cabinet-inside-position", cabinetIsOpen ? cabinetClosedPosition : cabinetOpenPosition);
-  cabinetIsOpen = !cabinetIsOpen;
-  
-})
 
-console.log(cabinetFront)
+
+const cabinetFronts = document.getElementsByClassName("cabinet-front");
+const cabinetsAreOpen = Array(cabinetFronts.length).fill(false)
+
+for (let i = 0; i < cabinetFronts.length; i++) {
+  cabinetFronts.item(i).addEventListener("click", () => {
+    r.style.setProperty("--cabinet-inside-position", cabinetsAreOpen[i] ? cabinetClosedPosition : cabinetOpenPosition);
+    cabinetsAreOpen[i] = !cabinetsAreOpen[i];
+
+    if(previousFolder < folders.length) {
+      folders.item(previousFolder).classList.remove("selected");
+    }  
+  })
+}
+
+const folders = document.getElementsByClassName("folder");
+
+let previousFolder = folders.length;
+for(let i = 0; i < folders.length; i++)
+  {
+    folders.item(i).addEventListener("click",() => {
+      if(previousFolder < folders.length) {
+        folders.item(previousFolder).classList.remove("selected");
+      }  
+      if(previousFolder == i) {
+        folders.item(i).classList.remove("selected");
+        previousFolder = folders.length;
+      }
+      else{
+        folders.item(i).classList.add("selected");
+        previousFolder = i;
+      }
+      
+    })
+  }

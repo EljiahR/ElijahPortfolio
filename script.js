83bcf5893cf5f6c7556277d1918f1638797f76db
EljiahR <142916290+EljiahR@users.noreply.github.com>
2024-07-31 14:39:32 +0000

    Fixed previously broken css, fixed hero z-indexing, added margins to project div to stop window from expanding and shrinking with cabinets, labels only tabbable when cabinet is open, when screen is too small for multiple cabinets only one will be open at a time

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
 const r = document.querySelector(":root");
 const cabinetClosedPosition = "-470px";
 const cabinetOpenPosition = "0px";
-
+const mediaQuery = window.matchMedia("(max-width: 1067px)");
 
 const cabinetFronts = document.getElementsByClassName("cabinet-front");
 const cabinetsAreOpen = Array(cabinetFronts.length).fill(false)
+const labelsByCabinet = Array(cabinetFronts.length);
+for (let i = 0; i < labelsByCabinet.length; i++) {
+  labelsByCabinet[i] = cabinetFronts.item(i).parentElement.getElementsByClassName("label");
+}
 
-console.log(cabinetFronts);
-console.log(cabinetsAreOpen);
+const closeCabinet = (cabinetFront, labels) => {
+    cabinetFront.classList.remove("open");
+    for(let i = 0; i < labels.length; i++){
+      labels.item(i).removeAttribute("tabIndex");
+    }
+}
+
+const openCabinet = (cabinetFront, labels) => {
+    cabinetFront.classList.add("open");
+  for (let i = 0; i < labels.length; i++) {
+    labels.item(i).tabIndex = "0";
+  }
+}
 
 for (let i = 0; i < cabinetFronts.length; i++) {
   cabinetFronts.item(i).addEventListener("click", () => {
     cabinetsAreOpen[i] = !cabinetsAreOpen[i];
     if (cabinetFronts.item(i).classList.contains("open")) {
-      cabinetFronts.item(i).classList.remove("open");
+      closeCabinet(cabinetFronts.item(i), labelsByCabinet[i]);
     } else {
-      cabinetFronts.item(i).classList.add("open");
+      openCabinet(cabinetFronts.item(i), labelsByCabinet[i]);
+      if (mediaQuery.matches) {
+        for (let j = 0; j < cabinetFronts.length; j++) {
+          if(j != i) {
+            closeCabinet(cabinetFronts.item(j), labelsByCabinet[j]);
+          }
+        }
+      }
     }
-    if(previousFolder < folders.length) {
+    if (previousFolder < folders.length) {
       folders.item(previousFolder).classList.remove("selected");
-    }  
+    }
     console.log(cabinetFronts);
     console.log(cabinetsAreOpen);
   })
@@ -28,20 +50,19 @@ for (let i = 0; i < cabinetFronts.length; i++) {
 const folders = document.getElementsByClassName("folder");
 
 let previousFolder = folders.length;
-for(let i = 0; i < folders.length; i++)
-  {
-    folders.item(i).addEventListener("click",() => {
-      if(previousFolder < folders.length) {
-        folders.item(previousFolder).classList.remove("selected");
-      }  
-      if(previousFolder == i) {
-        folders.item(i).classList.remove("selected");
-        previousFolder = folders.length;
-      }
-      else{
-        folders.item(i).classList.add("selected");
-        previousFolder = i;
-      }
-      
-    })
-  }
+for (let i = 0; i < folders.length; i++) {
+  folders.item(i).addEventListener("click", () => {
+    if (previousFolder < folders.length) {
+      folders.item(previousFolder).classList.remove("selected");
+    }
+    if (previousFolder == i) {
+      folders.item(i).classList.remove("selected");
+      previousFolder = folders.length;
+    }
+    else {
+      folders.item(i).classList.add("selected");
+      previousFolder = i;
+    }
+
+  })
+}

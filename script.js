74a4dc0ff5496bbe8a99837977960e1dbd2622dd
EljiahR <142916290+EljiahR@users.noreply.github.com>
2024-08-13 21:47:57 +0000

    Added in projects, bunch more css

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ const r = document.querySelector(":root");
 
 const contactDiv = document.getElementById("contact-div")
 
-const cabinetClosedPosition = "-470px";
-const cabinetOpenPosition = "0px";
+
 const mediaQuery = window.matchMedia("(max-width: 1067px)");
+
+/*
+const csProjects = [document.getElementById("cs-1"), document.getElementById("cs-2"), document.getElementById("cs-3"), document.getElementById("cs-4"), document.getElementById("cs-5"), document.getElementById("cs-6")]
+
+const webdevProjects = [document.getElementById("webdev-1"), document.getElementById("webdev-2"), document.getElementById("webdev-3"), document.getElementById("webdev-4"), document.getElementById("webdev-5"), document.getElementById("webdev-6"), document.getElementById("webdev-7"), document.getElementById("webdev-8"), document.getElementById("webdev-9")]
+*/
+
+let currentFolder;
+let currentProject;
 
 const cabinetFronts = document.getElementsByClassName("cabinet-front");
 const cabinetsAreOpen = Array(cabinetFronts.length).fill(false)
@@ -13,19 +21,23 @@ for (let i = 0; i < labelsByCabinet.length; i++) {
   labelsByCabinet[i] = cabinetFronts.item(i).parentElement.getElementsByClassName("label");
 }
 
+const projects = document.getElementsByClassName("project");
+
 const closeCabinet = (cabinetFront, labels) => {
-    cabinetFront.classList.remove("open");
-    for(let i = 0; i < labels.length; i++){
-      labels.item(i).removeAttribute("tabIndex");
-    }
+  cabinetFront.classList.remove("open");
+  for (let i = 0; i < labels.length; i++) {
+    labels.item(i).removeAttribute("tabIndex");
+  }
 }
 
 const openCabinet = (cabinetFront, labels) => {
-    cabinetFront.classList.add("open");
+  cabinetFront.classList.add("open");
   for (let i = 0; i < labels.length; i++) {
     labels.item(i).tabIndex = "0";
   }
 }
+
+const folders = document.getElementsByClassName("folder");
 
 for (let i = 0; i < cabinetFronts.length; i++) {
   cabinetFronts.item(i).addEventListener("click", () => {
@@ -36,7 +48,7 @@ for (let i = 0; i < cabinetFronts.length; i++) {
       openCabinet(cabinetFronts.item(i), labelsByCabinet[i]);
       if (mediaQuery.matches) {
         for (let j = 0; j < cabinetFronts.length; j++) {
-          if(j != i) {
+          if (j != i) {
             closeCabinet(cabinetFronts.item(j), labelsByCabinet[j]);
           }
         }
@@ -44,25 +56,31 @@ for (let i = 0; i < cabinetFronts.length; i++) {
     }
     if (previousFolder < folders.length) {
       folders.item(previousFolder).classList.remove("selected");
+      projects.item(previousFolder).classList.remove("selected");
     }
 
   })
 }
 
-const folders = document.getElementsByClassName("folder");
+
 
 let previousFolder = folders.length;
 for (let i = 0; i < folders.length; i++) {
   folders.item(i).addEventListener("click", () => {
     if (previousFolder < folders.length) {
       folders.item(previousFolder).classList.remove("selected");
+      projects.item(previousFolder).classList.remove("selected");
     }
-    if (previousFolder == i) {
+    if (previousFolder == i && folders.item(i).classList.contains("selected")) {
       folders.item(i).classList.remove("selected");
+      projects.item(i).classList.remove("selected");
       previousFolder = folders.length;
     }
     else {
       folders.item(i).classList.add("selected");
+      projects.item(i).classList.add("selected");
+      currentFolder = folders.item(i);
+      currentProject = projects.item(i);
       previousFolder = i;
     }
 
@@ -70,18 +88,39 @@ for (let i = 0; i < folders.length; i++) {
 }
 
 const scrollToTop = () => {
-  window.scrollTo({top: 0, behavior: "smooth"});
+  window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
 const toggleContact = () => {
-  if(contactDiv.style.display == "block") {
+  if (contactDiv.style.display == "block") {
     contactDiv.style.display = "none";
   } else {
-    contactDiv.style.display = "block"
+    contactDiv.style.display = "block";
   }
-  
+
 }
 
 contactDiv.addEventListener("click", e => {
-  if(e.target == contactDiv) toggleContact();
+  if (e.target == contactDiv) toggleContact();
 })
+
+const projectList = document.getElementById("project-list");
+
+const toggleProjectList = () => {
+  if (projectList.classList.contains("reveal-list")) {
+    projectList.classList.remove("reveal-list");
+  } else {
+    projectList.classList.add("reveal-list");
+  }
+}
+
+const projectOverlay = document.getElementById("project-overlay");
+
+const toggleProjectOverlay = (e) => {
+  if (e.target == projectOverlay) {
+    currentFolder.classList.remove("selected");
+    if (currentProject) currentProject.classList.remove("selected")
+  }
+}
+
+projectOverlay.addEventListener("click", toggleProjectOverlay);
